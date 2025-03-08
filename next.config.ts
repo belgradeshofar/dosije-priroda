1c1592c664b2863957ea47ce3381695854266391
BelgradeShofar <milospetrovictekstoteka@gmail.com>
2025-03-08 19:14:14 +0100

    ignorisanje

--- next.config.ts
+++ next.config.ts
@@ -5,3 +5,10 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+
+module.exports = {
+  typescript: {
+    // Upozorenje: Ovo omogućava da build uspešno prođe čak i ako postoje TypeScript greške.
+    ignoreBuildErrors: true,
+  },
+};

f1c436a5966231b892ad7a7f9d18b00d54e80792
BelgradeShofar <milospetrovictekstoteka@gmail.com>
2025-03-25 09:12:37 +0100

    Pokusaj

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -3,6 +3,10 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_KEY;
 
+// Testni logovi:
+console.log("SUPABASE URL (build-time):", supabaseUrl);
+console.log("SUPABASE KEY (build-time):", supabaseKey);
+
 if (!supabaseUrl || !supabaseKey) {
   throw new Error("Supabase URL and Key must be provided.");
 }
